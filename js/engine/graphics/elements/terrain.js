61ab0c83539295dc6881bc5f2fdb225950c8b4c0
Chris Kruining <chris@kruining.eu>
2018-12-25 12:44:48 +0100

    tweaked the terrain rendering a bit

--- js/engine/graphics/elements/terrain.js
+++ js/engine/graphics/elements/terrain.js
@@ -12,9 +12,9 @@ export default class Terrain
     constructor(renderer, size, view, projection)
     {
         this.size = size;
-        
+
         let load = p => fetch(p, {credentials: 'same-origin'}).then(r => r.text());
-    
+
         Promise.all([
             load('vertex.glsl'),
             load('fragment.glsl'),
@@ -25,10 +25,10 @@ export default class Terrain
                 new Vertex(renderer, v),
                 new Fragment(renderer, f)
             );
-            
+
             this.buffer = [];
             this.indices = [];
-            
+
             let biome = [
                 [ 0, 130,  76,   6 ],
                 [.3,  78, 168,  15 ],
@@ -70,34 +70,34 @@ export default class Terrain
                     color(vertices[1].y),
                     color(vertices[1].y),
                 ];
-                
+
                 vertices.forEach(v => {
                     v.x += (perlin(v.y, v.z) - .5) / 2;
                     v.z += (perlin(v.x, v.y) - .5) / 2;
                     v.y *= a;
                 });
-                
+
                 let normals = [
                     Vector3.normalFromPoints(vertices[indices[0]], vertices[indices[1]], vertices[indices[2]]),
                     Vector3.normalFromPoints(vertices[indices[3]], vertices[indices[4]], vertices[indices[5]]),
                 ];
-                
+
                 return { vertices, colors, normals };
             };
             let lastRow = [];
-            
+
             for(let z = 0.0; z < size.y; z++)
             {
                 for(let x = 0.0; x < size.x; x++)
                 {
                     let indices = [];
-                    
+
                     // buffer indices
                     {
                         let i = (col, row) => {
                             let doubleRows = Math.min(row, size.y - 1) * (size.x - 1);
                             let column = col * (row < size.y - 1 ? 2 : 1);
-                            
+
                             return row * (size.x + 1) + doubleRows + column;
                         };
                         let vertices = [
@@ -106,59 +106,89 @@ export default class Terrain
                             i(x, z + 1),     // bottom-left
                             i(x, z + 1) + 1, // bottom-right
                         ];
-                        
-                        switch((z % 2) * 2 + x % 2)
+
+                        if(z < size.y - 1)
                         {
-                            case 0:
-                                indices = [
-                                    0, 2, 3,
-                                    1, 3, 0,
-                                ];
-                                break;
-                            case 1:
-                                indices = [
-                                    0, 2, 1,
-                                    1, 3, 2,
-                                ];
-                                break;
-                            case 2:
-                                indices = [
-                                    1, 3, 2,
-                                    0, 1, 2,
-                                ];
-                                break;
-                            case 3:
-                                indices = [
-                                    1, 3, 0,
-                                    0, 2, 3,
-                                ];
-                                break;
+                            switch((z % 2) * 2 + x % 2)
+                            {
+                                case 0:
+                                    indices = [
+                                        0, 3, 2,
+                                        1, 3, 0,
+                                    ];
+                                    break;
+                                case 1:
+                                    indices = [
+                                        0, 1, 2,
+                                        1, 3, 2,
+                                    ];
+                                    break;
+                                case 2:
+                                    indices = [
+                                        1, 3, 2,
+                                        0, 1, 2,
+                                    ];
+                                    break;
+                                case 3:
+                                    indices = [
+                                        1, 3, 0,
+                                        0, 3, 2,
+                                    ];
+                                    break;
+                            }
                         }
-    
-                        if(z === size.y - 1)
+                        else
                         {
-                            let t = indices[0];
-                            indices[0] = indices[1];
-                            indices[1] = t;
+                            switch((z % 2) * 2 + x % 2)
+                            {
+                                case 0:
+                                    indices = [
+                                        3, 0, 2,
+                                        1, 3, 0,
+                                    ];
+                                    break;
+                                case 1:
+                                    indices = [
+                                        3, 2, 2,
+                                        1, 3, 2,
+                                    ];
+                                    break;
+                                case 2:
+                                    indices = [
+                                        1, 3, 2,
+                                        0, 1, 2,
+                                    ];
+                                    break;
+                                case 3:
+                                    indices = [
+                                        1, 3, 0,
+                                        0, 3, 2,
+                                    ];
+                                    break;
+                            }
+
+                                let t = indices[0];
+                                indices[0] = indices[1];
+                                indices[1] = t;
                         }
-    
+
                         // this.indices.push(indices.map(i => vertices[i]).join(','));
                         this.indices.push(...indices.map(i => vertices[i]));
                     }
-                    
+
                     // buffer vertices
                     {
                         let {vertices, colors, normals} = gridSquare(z, x, indices);
-    
+
                         // top-left
                         buffer(vertices[0], normals[0], colors[0]);
-    
+
                         if((z !== size.y - 1 || x === size.x - 1))
                         {
                             // top-right
                             buffer(vertices[1], normals[1], colors[1]);
                         }
-    
+
                         if(z === size.y - 1)
                         {
                             if(x === 0)
@@ -166,57 +196,57 @@ export default class Terrain
                                 // bottom-left
                                 lastRow.push([vertices[2], normals[0], colors[2]]);
                             }
-        
+
                             // bottom-right
                             lastRow.push([vertices[3], normals[1], colors[3]]);
                         }
                     }
                 }
             }
-            
+
             for(let x of lastRow)
             {
                 buffer(...x);
             }
-            
+
             let bv = new Buffer(renderer, [
                 [ this.program['vertex'], 3 ],
                 [ this.program['normal'], 3 ],
                 [ this.program['color'], 3 ],
             ]);
             bv.data = new Float32Array(this.buffer);
-            
+
             let bi = new Buffer(renderer, [], renderer.gl.ELEMENT_ARRAY_BUFFER);
             bi.data = new Uint16Array(this.indices);
-            
+
             let world = Matrix4.identity
                 .rotate(60, new Vector3(1, 0, 0))
                 .translate(new Vector3(-size.x / 2, 0, -size.y / 2));
-    
+
             this.program.world = world.points;
             this.program.view = view.points;
             this.program.projection = projection.points;
             this.program.lightDirection = new Vector3(.3, -1, .5).points;
             this.program.lightColour = new Vector3(1, .8, .8).points;
             this.program.lightBias = new Vector2(.3, .8).points;
-            
+
             this.loaded = true;
         });
     }
-    
+
     render(renderer)
     {
         if(this.loaded !== true)
         {
             return;
         }
-        
+
         // angle = performance.now() / 150 * Math.PI;
         // renderer.program.world = world
         //     .rotate(angle, new Vector3(0, 1, 0))
         //     .translate(new Vector3(-s.x / 2, 0.0, -s.y / 2))
         //     .points;
-    
+
         // renderer.gl.drawArrays(renderer.gl.TRIANGLES, 0, this.size.x * this.size.y / 3);
         renderer.gl.drawElements(renderer.gl.TRIANGLES, this.indices.length, renderer.gl.UNSIGNED_SHORT, 0);
     }
