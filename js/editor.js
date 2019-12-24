182d2dc3e7cae5a6699e22851c47009172c204cb
Chris Kruining <chris@kruining.eu>
2019-12-24 20:49:43 +0100

    why has this not been committed months ago...

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,13 @@
 import * as Fyn from '/node_modules/@fyn-software/component/fyn.js';
+import * as Types from '/node_modules/@fyn-software/data/types.js';
 import { Layout } from '/node_modules/@fyn-software/suite/js/common/layout/docks.js';
 import { Direction } from '/node_modules/@fyn-software/suite/js/common/layout/resizable.js';
+
+const Sizes = Types.Enum.define({
+    mobile: { icon: 'mobile-alt', x: '360px', y: '680px' },
+    tablet: { icon: 'tablet-alt', x: '768px', y: '1024px' },
+    desktop: { icon: 'desktop', x: '1920px', y: '1080px' },
+});
 
 export default class Editor extends Fyn.Component
 {
@@ -22,6 +29,7 @@ export default class Editor extends Fyn.Component
                     [ 1, 7 ],
                 ],
             }),
+            sizes: Sizes,
         };
     }
 }
